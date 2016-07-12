807dd8975d449938f06ad26b17a53dc530856e4c
franco-melandri <franco.melandri@yoox.com>
2016-07-12 11:36:50 +0200

    Added visitor example

---
+++ src/05-visitor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // var employees = new Employees();
+            var employees = new EmployeesNoIf();
+
+            Console.WriteLine("Report");
+            Console.WriteLine (employees.generate());
+            Console.ReadKey();
+        }
+    }
+}
