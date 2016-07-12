eb6d03fd5b1a2c5827dbd1de38061f0f7b9e189a
franco-melandri <franco.melandri@yoox.com>
2016-07-12 10:54:51 +0200

    Polymorphims insteda of condition

---
+++ src/04-polymorphism/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var shapes = new Shape[] {
+                new Square(10),
+                new Circle(5)
+            };
+
+            var area = new AreaCalculator (shapes);
+            Console.WriteLine("Area = {0}", area.Sum());
+
+            // var shapesNoIf = new ShapeNoIf[] {
+            //     new SquareNoIf(10),
+            //     new CircleNoIf(5)
+            // };
+
+            // var areaNoIf = new AreaCalculatorNoIf (shapesNoIf);
+            // Console.WriteLine("Area = {0}", areaNoIf.Sum());
+
+            Console.ReadKey();
+        }
+    }
+}
