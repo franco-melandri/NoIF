edfe6e0c6be99343e52163de1b26de3429be07f6
Francesco Chiusolo <chiusolofrancesco@gmail.com>
2018-10-29 22:20:43 +0100

    Setup files

---
+++ src/swift/nullObject/Tests/nullObjectTests/nullObjectTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import nullObject
+
+final class NullObjectTests: XCTestCase {
+
+    func testSomething() {
+        // let sut = fizz()
+        // let result = sut.getResult(3)
+        // XCTAssertEqual(result, "fizz")
+    }
+
+    static var allTests = [
+        ("testSomething", testSomething),
+    ]
+}
